3c0ffed125dde70ffefff952ff28b1b87b9f0d5f
Gunavadhi22 <gunavadhi9303@mountzion.ac.in>
2026-02-20 15:46:06 +0530

    server.js

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+// Serve static files from public folder
 app.use(express.static("public"));
 
+// Home route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const PORT = 3000;
+// IMPORTANT: Use Render's PORT
+const PORT = process.env.PORT || 3000;
 
+// Start server
 app.listen(PORT, () => {
-  console.log("Server running at http://localhost:3000");
+  console.log("Server running on port " + PORT);
 });
